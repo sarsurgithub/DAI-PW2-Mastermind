dbcb293c0eacc8209926c7f36a1cbd4c37e0f6f3
Jonas Troeltsch <jonas.troeltsch@heig-vd.ch>
2023-11-27 15:28:47 +0100

    rework Client + adapted to updated protocol

--- src/main/java/heig/vd/dai/client/Client.java
+++ src/main/java/heig/vd/dai/client/Client.java
@@ -2,9 +2,11 @@ package heig.vd.dai.client;
 
 import picocli.CommandLine;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 @CommandLine.Command(name = "client", version = "1.0", mixinStandardHelpOptions = true)
@@ -20,94 +22,151 @@ public class Client implements Runnable {
             description = "Host (default: ${DEFAULT-VALUE})")
     private String host;
 
-    Scanner scanner = new Scanner(System.in);
+    private final Scanner scanner = new Scanner(System.in);
+    private boolean gameInProgress = false;
+    private int nbPins = 4;
+
     @Override
     public void run() {
         try (Socket socket = new Socket(host, port);
              PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            ) {
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+        ) {
 
             String fromServer;
             String fromUser;
 
             System.out.println("Connected to server. Type 'START' to begin a game, 'RULES' for rules, 'HELP' for commands, 'QUIT' to quit.");
 
-            while ((fromServer = in.readLine()) != null) {
-                System.out.println("Server: " + fromServer);
-
-                if (fromServer.startsWith("ANSWER")) {
-                    processAnswer(fromServer);
-                } else {
-                    switch (fromServer) {
-                        case "FINISHED WON":
-                            System.out.println("You won! Type 'START' to play again or 'QUIT' to exit.");
-                            break;
-                        case "FINISHED LOST":
-                            System.out.println("Game Over, you lost. Type 'START' to play again or 'QUIT' to exit.");
-                            break;
-                    }
-                }
-                System.out.print("Enter command: ");
-                fromUser = scanner.nextLine();
-
-                if ("QUIT".equalsIgnoreCase(fromUser)) {
-                    out.println(fromUser);
+            while (true) {
+                try {
+                    fromServer = in.readLine();
+                } catch (IOException e) {
+                    System.out.println("Server disconnected.");
                     break;
                 }
 
-                String processedInput = processInput(fromUser);
-                if (!processedInput.isEmpty()) {
-                    out.println(processedInput);
+                if (fromServer == null){
+                    System.out.println("Server disconnected.");
+                    break;
+                }
+
+                String[] serverParts = fromServer.split(" ");
+                processServerMessage(serverParts);
+
+                boolean correctInput = false;
+
+                do {
+                    System.out.print("Enter command: ");
+                    fromUser = scanner.nextLine().toUpperCase();
+                    String[] userParts = fromUser.split(" ");
+
+                    switch (userParts[0]) {
+                        case "START":
+                            fromUser = processStartCommand(userParts);
+                            if (fromUser != null) {
+                                correctInput = true;
+                            }
+                            break;
+                        case "TRY":
+                            if (gameInProgress) {
+                                correctInput = processTryInput(userParts);
+                            } else {
+                                System.out.println("No game in progress. Type 'START' to start a new game.");
+                            }
+                            break;
+                        case "RULES", "HELP", "QUIT":
+                            if (userParts.length == 1) {
+                                correctInput = true;
+                            } else {
+                                System.out.println("Invalid command. Type 'HELP' for a list of commands.");
+                            }
+                            break;
+                        default:
+                            System.out.println("Invalid command. Type 'HELP' for a list of commands.");
+                    }
+                } while (!correctInput);
+
+                if (!fromUser.isEmpty()) {
+                    out.println(fromUser);
                 }
             }
 
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Error connecting to server: " + e.getMessage());
         }
     }
 
-    private String processInput(String input) {
-        // Process the user input here based on the expected command format
-        // Return the processed/validated input, or an empty string if invalid
-        input = input.toUpperCase();
-        //TRY command processing
-        if (input.startsWith("TRY")) {
-            String[] parts = input.split(" ");
-            if (parts.length == 2 && parts[1].matches("[RBGY]{4}")) {
-                return input;
-            } else {
-                System.out.println("Invalid TRY command format. Example: 'TRY RRGB'");
-                return "";
-            }
-        }
-
-        // START command processing
-        if (input.equalsIgnoreCase("START")) {
-            System.out.println("Do you want to play a default[1] or custom[2] game?");
-            try {
-                int type = readIntFromUser(scanner);
-                switch (type) {
-                    case 1:
-                        return input;
-                    case 2:
-                        System.out.println("How many tries do you want?");
-                        int tries = readIntFromUser(scanner);
-                        System.out.println("How many colors do you want?");
-                        int colors = readIntFromUser(scanner);
-                        return input + " " + tries + " " + colors;
-                    default:
-                        System.out.println("This was not a valid answer, please start again.");
-                        return "";
+    private void processServerMessage(String[] serverParts) {
+        switch (serverParts[0]) {
+            case "OK":
+                break;
+            case "ANSWER":
+                processAnswer(serverParts);
+                break;
+            case "STARTED":
+                System.out.println("Game started. Type 'TRY' to make a guess or 'QUIT' to exit.");
+                gameInProgress = true;
+                break;
+            case "SEND":
+                if (serverParts.length > 1) {
+                    String s = String.join(" ", serverParts);
+                    System.out.println(s);
+                } else {
+                    System.out.println("Invalid SEND from server");
                 }
-            } catch (InputMismatchException e) {
-                System.out.println("Invalid input. Please enter a number.");
-                scanner.nextLine(); // Clear the invalid input
-                return "";
+                break;
+            case "FINISHED":
+                switch (serverParts[1]) {
+                    case "WON":
+                        System.out.println("You won! Type 'START' to play again or 'QUIT' to exit.");
+                        gameInProgress = false;
+                        break;
+                    case "LOST":
+                        System.out.println("Game Over, you lost. Type 'START' to play again or 'QUIT' to exit.");
+                        gameInProgress = false;
+                        break;
+                    default:
+                        System.out.println("Invalid game result received from server.");
+                }
+                break;
+            case "ERROR":
+                System.out.println("Error received from server: " + serverParts[1]);
+                break;
+        }
+    }
+
+    private String processStartCommand(String[] userParts) {
+        if (userParts.length == 1) {
+            System.out.println("Do you want to play a default[1] (10 tries & 4 pins) or custom[2] game?");
+
+            int type = readIntFromUser(scanner);
+            switch (type) {
+                case 1:
+                    return userParts[0];
+                case 2:
+                    System.out.println("How many tries do you want?");
+                    int tries = readIntFromUser(scanner);
+                    System.out.println("How many pins do you want?");
+                    this.nbPins = readIntFromUser(scanner);
+                    return userParts[0] + " " + tries + " " + nbPins;
+                default:
+                    System.out.println("This was not a valid answer, please start again.");
+                    return null;
             }
         }
+        System.out.println("Invalid START command format. It should be 'START'");
+        return null;
+    }
 
-        return input.toUpperCase(); // Default processing
+    private boolean processTryInput(String[] parts) {
+        //TRY command processing has to be length this.pins
+        if (parts.length == 2 && parts[1].length() == this.nbPins && parts[1].matches("[RGBY]+")) {
+            return true;
+        }
+        System.out.println("Invalid TRY command format. It should be " + this.nbPins + " characters long and only contain the letters RGBY.");
+        return false;
+
     }
 
 
@@ -123,15 +182,16 @@ public class Client implements Runnable {
     }
 
 
-    private static void processAnswer(String answer) {
-        String[] parts = answer.split(" ");
-        if (parts.length == 3) {
+    private static void processAnswer(String[] answerParts) {
+
+        if (answerParts.length == 4) {
             try {
-                int clue1 = Integer.parseInt(parts[1]);
-                int clue2 = Integer.parseInt(parts[2]);
+                int clue1 = Integer.parseInt(answerParts[1]);
+                int clue2 = Integer.parseInt(answerParts[2]);
                 // Process the clues. For example, display them to the user.
                 System.out.println("Number of colors correctly placed: " + clue1 + "\n" +
-                        "Numbers of colors present in the answer: " + clue2);
+                        "Numbers of colors present in the answer: " + clue2 + "\n" +
+                        "Number of tries left: " + answerParts[3]);
             } catch (NumberFormatException e) {
                 System.out.println("Error processing clues: " + e.getMessage());
             }
